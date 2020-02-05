439820db71cd3bedd2b34c5e4972582ac35fa85e
felipemuner <felipe.muner@gmail.com>
2020-02-05 13:02:07 +0700

    cost center

--- routes/costCenter.js
+++ routes/costCenter.js
@@ -5,6 +5,7 @@ const cc = require(process.env.PWD + '/models/CostCenter')
 
 /* GET home page. */
 router.get("/", cc.getAll, (req, res, next) => {
+  console.log('to aq');
   res.json(req.costCenter)
 }).post("/save", cc.save, cc.getAll, (req, res, next) => {
   res.json(req.costCenter)
